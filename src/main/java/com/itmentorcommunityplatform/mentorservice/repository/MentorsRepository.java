80c47943d97dd72c36faddab327f150177f53845
EGladki <e.gladki@gmail.com>
2026-02-27 15:54:32 +0300

    feat: добавил логику обновления ментора в таблице mentors при аутентификации - при telegram_username = null -> оставляем ранее сохраненное значение telegram_url - обнавляем статус is_active

--- src/main/java/com/itmentorcommunityplatform/mentorservice/repository/MentorsRepository.java
+++ src/main/java/com/itmentorcommunityplatform/mentorservice/repository/MentorsRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MentorsRepository extends CrudRepository<Mentor, Long> {
 
@@ -21,8 +23,6 @@ public interface MentorsRepository extends CrudRepository<Mentor, Long> {
     void upsertMentor(@Param("telegramUserId") Long telegramUserId,
                       @Param("telegramUrl") String telegramUrl);
 
-
-
     @Query("""
             INSERT INTO guaranteed_reviews_prices (MENTOR_ID, PROJECT_TYPE, LANGUAGE, PRICE_USD)
                 VALUES ((select id from mentors where mentor_telegram_user_id= :telegramUserId), :projectType, :language, :price)
@@ -31,7 +31,18 @@ public interface MentorsRepository extends CrudRepository<Mentor, Long> {
                 RETURNING (xmax = 0) AS inserted
             """)
     boolean updatePriceForGuaranteedReviews(@Param("price") int price,
-                                         @Param("projectType") String projectType,
-                                         @Param("telegramUserId") Long telegramUserId,
-                                         @Param("language") String language);
+                                            @Param("projectType") String projectType,
+                                            @Param("telegramUserId") Long telegramUserId,
+                                            @Param("language") String language);
+
+    Optional<Mentor> getMentorByMentorTelegramUserId(Long mentorTelegramUserId);
+
+    @Query("""
+            UPDATE mentor_service.mentors
+            SET telegram_url = :telegramUrl,
+                is_active = :isActive
+            WHERE mentor_telegram_user_id = :telegramUserId
+            RETURNING id, mentor_telegram_user_id, telegram_url, is_active;
+            """)
+    Mentor updateMentor(Long telegramUserId, String telegramUrl, boolean isActive);
 }
--- src/main/java/com/itmentorcommunityplatform/mentorservice/service/MentorService.java
+++ src/main/java/com/itmentorcommunityplatform/mentorservice/service/MentorService.java
@@ -1,8 +1,10 @@
 package com.itmentorcommunityplatform.mentorservice.service;
 
+import com.itmentorcommunityplatform.mentorservice.domain.Mentor;
 import com.itmentorcommunityplatform.mentorservice.domain.type.UpsertResult;
 import com.itmentorcommunityplatform.mentorservice.dto.AddPriceForGuaranteedReviewRequest;
 import com.itmentorcommunityplatform.mentorservice.dto.ProfileWithTelegramIdDto;
+import com.itmentorcommunityplatform.mentorservice.dto.event.UserAuthenticatedEvent;
 import com.itmentorcommunityplatform.mentorservice.httpclient.ServiceHttpClient;
 import com.itmentorcommunityplatform.mentorservice.repository.MentorsRepository;
 import com.itmentorcommunityplatform.mentorservice.validator.ProjectTypeValidator;
@@ -14,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,15 +28,18 @@ public class MentorService {
     private final MentorsRepository mentorsRepository;
     private final ServiceHttpClient httpClient;
 
+    private static String resolveTelegramUrl(UserAuthenticatedEvent event, Mentor mentor) {
+        return event.getTelegramUsername() != null && !event.getTelegramUsername().isBlank() ?
+                "https://t.me/" + event.getTelegramUsername() : mentor.getTelegramUrl();
+    }
+
     @Transactional
     public UpsertResult upsertMentorAndGuaranteedReviewsPrices(AddPriceForGuaranteedReviewRequest request) {
-
-
         telegramUrlValidator.validate(request.telegramUrl());
         projectTypeValidator.validate(request.projectType());
 
         ProfileWithTelegramIdDto responseDto = httpClient.getProfileByTgUrl(request.telegramUrl())
-                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
                         "Profile with given Telegram URL not found"));
 
         mentorsRepository.upsertMentor(responseDto.telegramUserId(), request.telegramUrl());
@@ -45,5 +51,23 @@ public class MentorService {
         return inserted ? UpsertResult.CREATED : UpsertResult.UPDATED;
     }
 
+    @Transactional
+    public void updateMentorProfile(UserAuthenticatedEvent event) {
+        boolean isActive = event.getRoles().contains("MENTOR");
+        Optional<Mentor> possibleMentor = mentorsRepository.getMentorByMentorTelegramUserId(event.getTelegramUserId());
+        if (possibleMentor.isEmpty()) {
+            log.info("Mentor with with telegram id: {} not found", event.getTelegramUserId());
+            return;
+        }
 
+        Mentor mentor = possibleMentor.get();
+        String telegramUrl = resolveTelegramUrl(event, mentor);
+        if (mentor.getTelegramUrl().equals(telegramUrl) && mentor.isActive() == isActive) {
+            log.info("Nothing to update for mentor with telegram id: {}", mentor.getMentorTelegramUserId());
+            return;
+        }
+
+        Mentor updated = mentorsRepository.updateMentor(event.getTelegramUserId(), telegramUrl, isActive);
+        log.info("Mentor with telegram id: {} has been updated", updated.getMentorTelegramUserId());
+    }
 }
