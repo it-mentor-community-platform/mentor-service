79b0e31dd0c0c04f5d9d670b49e7da8176c9635c
EGladki <e.gladki@gmail.com>
2026-03-05 14:24:57 +0300

    feat: rest-point по добавлению/обновлению ментора и его описания Валидация запроса через аннотации, Добавил обработку исключения валидации через GlobalExceptionHandler Upsert ментора, его описания, языков и его сервисов

--- src/main/java/com/itmentorcommunityplatform/mentorservice/repository/MentorsRepository.java
+++ src/main/java/com/itmentorcommunityplatform/mentorservice/repository/MentorsRepository.java
@@ -1,34 +1,47 @@
 package com.itmentorcommunityplatform.mentorservice.repository;
 
 import com.itmentorcommunityplatform.mentorservice.domain.Mentor;
+import com.itmentorcommunityplatform.mentorservice.domain.MentorDescription;
+import com.itmentorcommunityplatform.mentorservice.dto.MentorUpsertResult;
 import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MentorsRepository extends CrudRepository<Mentor, Long> {
 
-    @Modifying
     @Query("""
             INSERT INTO mentors (mentor_telegram_user_id, telegram_url, is_active)
                 VALUES (:telegramUserId, :telegramUrl, TRUE)
                 ON CONFLICT (mentor_telegram_user_id)
                 DO UPDATE SET telegram_url = EXCLUDED.telegram_url,
                 is_active   = TRUE
+                RETURNING id, mentor_telegram_user_id, telegram_url, is_active, (xmax = 0) AS inserted;
             """)
-    void upsertMentor(@Param("telegramUserId") Long telegramUserId,
-                      @Param("telegramUrl") String telegramUrl);
+    MentorUpsertResult upsertMentor(@Param("telegramUserId") Long telegramUserId,
+                                    @Param("telegramUrl") String telegramUrl);
+
+
+    @Query("""
+            UPDATE mentors
+            SET telegram_url = :telegramUrl,
+                is_active = :isActive
+            WHERE mentor_telegram_user_id = :telegramUserId
+            RETURNING id, mentor_telegram_user_id, telegram_url, is_active;
+            """)
+    Mentor updateMentor(Long telegramUserId, String telegramUrl, boolean isActive);
 
     @Query("""
             INSERT INTO guaranteed_reviews_prices (MENTOR_ID, PROJECT_TYPE, LANGUAGE, PRICE_USD)
                 VALUES ((select id from mentors where mentor_telegram_user_id= :telegramUserId), :projectType, :language, :price)
                 ON CONFLICT (mentor_id, project_type, language)
                 DO UPDATE SET price_usd = EXCLUDED.price_usd
-                RETURNING (xmax = 0) AS inserted
+                RETURNING (xmax = 0) AS inserted;
             """)
     boolean updatePriceForGuaranteedReviews(@Param("price") int price,
                                             @Param("projectType") String projectType,
@@ -38,11 +51,13 @@ public interface MentorsRepository extends CrudRepository<Mentor, Long> {
     Optional<Mentor> getMentorByMentorTelegramUserId(Long mentorTelegramUserId);
 
     @Query("""
-            UPDATE mentor_service.mentors
-            SET telegram_url = :telegramUrl,
-                is_active = :isActive
-            WHERE mentor_telegram_user_id = :telegramUserId
-            RETURNING id, mentor_telegram_user_id, telegram_url, is_active;
+            INSERT INTO mentor_descriptions (mentor_user_id, name, cost, description)
+                VALUES (:mentorUserId, :name, :cost, :description)
+                ON CONFLICT (mentor_user_id)
+                DO UPDATE SET name = EXCLUDED.name,
+                              cost = EXCLUDED.cost,
+                              description = EXCLUDED.description
+                RETURNING id, mentor_user_id, name, cost, description;
             """)
-    Mentor updateMentor(Long telegramUserId, String telegramUrl, boolean isActive);
+    MentorDescription upsertMentorDescription(Long mentorUserId, String name, String cost, String description);
 }
--- src/main/java/com/itmentorcommunityplatform/mentorservice/service/MentorService.java
+++ src/main/java/com/itmentorcommunityplatform/mentorservice/service/MentorService.java
@@ -2,11 +2,15 @@ package com.itmentorcommunityplatform.mentorservice.service;
 
 import com.itmentorcommunityplatform.mentorservice.domain.Mentor;
 import com.itmentorcommunityplatform.mentorservice.domain.type.UpsertResult;
+import com.itmentorcommunityplatform.mentorservice.dto.AddMentorWithDescriptionRequest;
 import com.itmentorcommunityplatform.mentorservice.dto.AddPriceForGuaranteedReviewRequest;
+import com.itmentorcommunityplatform.mentorservice.dto.MentorUpsertResult;
 import com.itmentorcommunityplatform.mentorservice.dto.ProfileWithTelegramIdDto;
 import com.itmentorcommunityplatform.mentorservice.dto.event.UserAuthenticatedEvent;
 import com.itmentorcommunityplatform.mentorservice.httpclient.ServiceHttpClient;
 import com.itmentorcommunityplatform.mentorservice.repository.MentorsRepository;
+import com.itmentorcommunityplatform.mentorservice.repository.ProgrammingLanguagesRepository;
+import com.itmentorcommunityplatform.mentorservice.repository.ServicesRepository;
 import com.itmentorcommunityplatform.mentorservice.validator.ProjectTypeValidator;
 import com.itmentorcommunityplatform.mentorservice.validator.TelegramUrlValidator;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +20,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -26,6 +31,8 @@ public class MentorService {
     private final TelegramUrlValidator telegramUrlValidator;
     private final ProjectTypeValidator projectTypeValidator;
     private final MentorsRepository mentorsRepository;
+    private final ProgrammingLanguagesRepository programmingLanguagesRepository;
+    private final ServicesRepository servicesRepository;
     private final ServiceHttpClient httpClient;
 
     @Transactional
@@ -42,6 +49,29 @@ public class MentorService {
                 request.projectType(),
                 responseDto.telegramUserId(),
                 request.language());
+
+        return inserted ? UpsertResult.CREATED : UpsertResult.UPDATED;
+    }
+
+    @Transactional
+    public UpsertResult upsertMentorWithDescription(AddMentorWithDescriptionRequest request) {
+        telegramUrlValidator.validate(request.telegramUrl());
+
+        MentorUpsertResult mentorUpsertResult = mentorsRepository.upsertMentor(
+                request.mentorTelegramUserId(),
+                request.telegramUrl());
+
+        boolean inserted = mentorUpsertResult.inserted();
+
+        mentorsRepository.upsertMentorDescription(
+                mentorUpsertResult.id(),
+                request.description().name(),
+                request.description().cost(),
+                request.description().description());
+
+        upsertLanguages(mentorUpsertResult.id(), request.programmingLanguages());
+        upsertServices(mentorUpsertResult.id(), request.services());
+        log.info("Mentor with telegram url: {} has been upserted", request.telegramUrl());
 
         return inserted ? UpsertResult.CREATED : UpsertResult.UPDATED;
     }
@@ -64,6 +94,18 @@ public class MentorService {
 
         Mentor updated = mentorsRepository.updateMentor(event.getTelegramUserId(), telegramUrl, isActive);
         log.info("Mentor with telegram id: {} has been updated", updated.getMentorTelegramUserId());
+    }
+
+    private void upsertLanguages(Long mentorId, List<String> languages) {
+        List<Long> languagesIds = languages.stream().map(programmingLanguagesRepository::upsertProgrammingLanguage).toList();
+        programmingLanguagesRepository.deleteMentorsProgrammingLanguages(mentorId);
+        languagesIds.forEach(languageId -> programmingLanguagesRepository.insertMentorsProgrammingLanguage(mentorId, languageId));
+    }
+
+    private void upsertServices(Long mentorId, List<String> services) {
+        List<Long> servicesIds = services.stream().map(servicesRepository::upsertServices).toList();
+        servicesRepository.deleteMentorsServices(mentorId);
+        servicesIds.forEach(serviceId -> servicesRepository.insertMentorsServices(mentorId, serviceId));
     }
 
     private static String resolveTelegramUrl(UserAuthenticatedEvent event, Mentor mentor) {
