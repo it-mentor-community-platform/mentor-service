0721868a9aa29ef8da6d160a33b8a037f3bd58a1
EGladki <e.gladki@gmail.com>
2026-03-05 14:31:47 +0300

    chore: рефакторинг, добавил swagger документацию, ручне http тесты

--- src/main/java/com/itmentorcommunityplatform/mentorservice/validator/TelegramUrlValidator.java
+++ src/main/java/com/itmentorcommunityplatform/mentorservice/validator/TelegramUrlValidator.java
@@ -9,10 +9,8 @@ public class TelegramUrlValidator {
     private static final Pattern TELEGRAM_PATTERN = Pattern.compile("^https://t\\.me/[^\\s/]+$");
 
     public void validate(String value) {
-
         if (value == null || !TELEGRAM_PATTERN.matcher(value).matches()) {
             throw new IllegalArgumentException("Telegram profile url incorrect");
         }
-
     }
 }
