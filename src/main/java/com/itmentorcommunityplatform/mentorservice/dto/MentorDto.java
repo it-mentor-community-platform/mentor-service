6c786ac66c0e9c2e3ae678f586e59d325080b73b
Lqwd <AppLoneQW@yandex.ru>
2026-06-08 00:02:14 +0300

    fix: ProjectCreatedEvent и MentorDto
    - ProjectCreatedEvent добавил отсутствующее поле authorTelegramUserId
    - Для MentorDto добавил SnakeCaseStrategy

--- src/main/java/com/itmentorcommunityplatform/mentorservice/dto/MentorDto.java
+++ src/main/java/com/itmentorcommunityplatform/mentorservice/dto/MentorDto.java
@@ -1,6 +1,9 @@
 package com.itmentorcommunityplatform.mentorservice.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record MentorDto(
         Long mentorTelegramUserId,
         String mentorTelegramProfileUrl
