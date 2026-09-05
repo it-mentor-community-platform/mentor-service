5701e14129d08971670dc8d1fdc207b90799f7c8
Касьянов Михаил Юрьевич <vtb4126802@corp.dev.vtb>
2026-09-05 19:28:30 +0500

    fix: handle missing request header

--- src/main/java/com/itmentorcommunityplatform/mentorservice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/itmentorcommunityplatform/mentorservice/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
